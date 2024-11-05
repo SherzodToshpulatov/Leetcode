8cbb8167fb2037a4cf0225778a5f3487b2d37c8a
SherzodToshpulatov <sherzodtoshpolatov5@gmail.com>
2024-11-05 16:50:03 +0500

    another solution

---
+++ Easy/Length of Last Word.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/length-of-last-word/
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+
+        return len(words[-1]) if words else 0
