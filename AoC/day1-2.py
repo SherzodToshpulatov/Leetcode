fc3db253a2fc69e70f769b7c1a0c6c9f746409c1
SherzodToshpulatov <sherzodtoshpolatov5@gmail.com>
2024-12-03 19:10:37 +0500

    part two

--- AoC/day1-2.py
+++ AoC/day1-2.py
@@ -1,20 +1,19 @@
+from collections import defaultdict, Counter
 
 with open("day1.txt", "r") as file:
     lines = file.readlines()
 left_list = []
-right_list = []
+right_list = Counter()
 
 for line in lines:
     left,right = line.split()
     left,right = int(left),int(right)
     left_list.append(left)
-    right_list.append(right)
+    right_list[right] += 1
 
-left = sorted(left_list)
-right = sorted(right_list)
+left_list = sorted(left_list)
 
 answer = 0
-for l, r in zip(left_list, right_list):
-    answer += abs(l-r)
-
+for l in left_list:
+    answer += l * right_list.get(l, 0)
 print(answer)
